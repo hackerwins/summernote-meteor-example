b9f308aea4b3da551c4204e86a0374d40d469c0d
Ankit Gaurav <ankitgaurav@outlook.com>
2016-03-12 00:18:58 +0530

    corrected selector name and removed extra stuff

--- summernote-meteor-example.js
+++ summernote-meteor-example.js
@@ -1,29 +1,7 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
   Template.editor.rendered = function () {
-    $('.summernote').summernote({
+    $('#summernote').summernote({
       height: 200
     });
   };
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
